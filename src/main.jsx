d09752e4935d9ffd506915423ab92dbb86222b59
Myroslav Shevchenko <myroslav4eg@gmail.com>
2026-04-24 12:26:34 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from '../weight-loss-plan-v4.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
